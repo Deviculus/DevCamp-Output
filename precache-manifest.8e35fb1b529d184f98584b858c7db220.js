8956b4d655087ff2887913f28b858a0c907c4d96
Ignacio Andred D. Villanueva <the.ignacionizer@gmail.com>
2019-04-21 23:52:08 +0800

    Updates

---
+++ precache-manifest.8e35fb1b529d184f98584b858c7db220.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b88ab6c304e2392fd8f",
+    "url": "/DevCamp-Output/static/js/runtime~main.17f4c539.js"
+  },
+  {
+    "revision": "bd85865d6e653edcba08",
+    "url": "/DevCamp-Output/static/js/main.f85bcdf6.chunk.js"
+  },
+  {
+    "revision": "0736c783ccd78473c993",
+    "url": "/DevCamp-Output/static/js/2.34a5ba11.chunk.js"
+  },
+  {
+    "revision": "bd85865d6e653edcba08",
+    "url": "/DevCamp-Output/static/css/main.8ddfc86d.chunk.css"
+  },
+  {
+    "revision": "391b65b0ff7b9f40d015395d532606c7",
+    "url": "/DevCamp-Output/index.html"
+  }
+];
